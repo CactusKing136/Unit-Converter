519a1a5c9eb4b497854daf41f9d3128bc430ca08
CactusKing136 <alexedwards2311@icloud.com>
2024-06-10 21:05:13 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const convertBtn = document.querySelector(".convert-btn")
+const inputEl = document.querySelector(".num-input")
+const lengthEl = document.querySelector(".length-output")
+const volumeEl = document.querySelector(".volume-output")
+const massEl = document.querySelector(".mass-output")
+const bodyEl = document.querySelector("body")
+const convertLength = 3.281
+const convertVolume = 0.246
+const convertMass = 2.204
+let lengthMeters = 0
+let lengthFeet = 0
+let volumeLiters = 0
+let volumeGallons = 0
+let massKilograms = 0
+let massPounds = 0
+
+convertBtn.addEventListener("click", () => {
+
+    lengthFeet = Math.floor((inputEl.value * convertLength) * 1000) / 1000
+    lengthMeters = Math.floor((inputEl.value / convertLength) * 1000) / 1000
+    lengthEl.textContent = `${inputEl.value} meters = ${lengthFeet} feet | ${inputEl.value} feet = ${lengthMeters} meters`
+
+    volumeGallons = Math.floor((inputEl.value * convertVolume) * 1000) / 1000
+    volumeLiters = Math.floor((inputEl.value / convertVolume) * 1000) / 1000
+    volumeEl.textContent = `${inputEl.value} liters = ${volumeGallons} gallons | ${inputEl.value} gallons = ${volumeLiters} liters`
+
+    massPounds = Math.floor((inputEl.value * convertLength) * 1000) / 1000
+    massKilograms = Math.floor((inputEl.value / convertLength) * 1000) / 1000
+    massEl.textContent = `${inputEl.value} kilograms = ${massPounds} pounds | ${inputEl.value} pounds = ${massKilograms} kilograms`
+
+})
+
+bodyEl.addEventListener("keypress", (event) => {
+
+    if (event.key === "Enter") {
+        lengthFeet = Math.floor((inputEl.value * convertLength) * 1000) / 1000
+        lengthMeters = Math.floor((inputEl.value / convertLength) * 1000) / 1000
+        lengthEl.textContent = `${inputEl.value} meters = ${lengthFeet} feet | ${inputEl.value} feet = ${lengthMeters} meters`
+
+        volumeGallons = Math.floor((inputEl.value * convertVolume) * 1000) / 1000
+        volumeLiters = Math.floor((inputEl.value / convertVolume) * 1000) / 1000
+        volumeEl.textContent = `${inputEl.value} liters = ${volumeGallons} gallons | ${inputEl.value} gallons = ${volumeLiters} liters`
+
+        massPounds = Math.floor((inputEl.value * convertLength) * 1000) / 1000
+        massKilograms = Math.floor((inputEl.value / convertLength) * 1000) / 1000
+        massEl.textContent = `${inputEl.value} kilograms = ${massPounds} pounds | ${inputEl.value} pounds = ${massKilograms} kilograms`
+    }
+    
+})
