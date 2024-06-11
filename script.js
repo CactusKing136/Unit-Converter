d702cd2be89c2c8bceee80ef99bc3d1fb3732dee
CactusKing136 <alexedwards2311@icloud.com>
2024-06-11 17:20:25 +0100

    Changed styling and input cleared after converted

--- script.js
+++ script.js
@@ -15,7 +15,22 @@ let massKilograms = 0
 let massPounds = 0
 
 convertBtn.addEventListener("click", () => {
+    calculate()
 
+    inputEl.value = ""
+})
+
+bodyEl.addEventListener("keypress", (event) => {
+
+    if (event.key === "Enter") {
+        calculate()
+
+        inputEl.value = ""
+    }
+    
+})
+
+const calculate = () => {
     lengthFeet = Math.floor((inputEl.value * convertLength) * 1000) / 1000
     lengthMeters = Math.floor((inputEl.value / convertLength) * 1000) / 1000
     lengthEl.textContent = `${inputEl.value} meters = ${lengthFeet} feet | ${inputEl.value} feet = ${lengthMeters} meters`
@@ -27,23 +42,4 @@ convertBtn.addEventListener("click", () => {
     massPounds = Math.floor((inputEl.value * convertLength) * 1000) / 1000
     massKilograms = Math.floor((inputEl.value / convertLength) * 1000) / 1000
     massEl.textContent = `${inputEl.value} kilograms = ${massPounds} pounds | ${inputEl.value} pounds = ${massKilograms} kilograms`
-
-})
-
-bodyEl.addEventListener("keypress", (event) => {
-
-    if (event.key === "Enter") {
-        lengthFeet = Math.floor((inputEl.value * convertLength) * 1000) / 1000
-        lengthMeters = Math.floor((inputEl.value / convertLength) * 1000) / 1000
-        lengthEl.textContent = `${inputEl.value} meters = ${lengthFeet} feet | ${inputEl.value} feet = ${lengthMeters} meters`
-
-        volumeGallons = Math.floor((inputEl.value * convertVolume) * 1000) / 1000
-        volumeLiters = Math.floor((inputEl.value / convertVolume) * 1000) / 1000
-        volumeEl.textContent = `${inputEl.value} liters = ${volumeGallons} gallons | ${inputEl.value} gallons = ${volumeLiters} liters`
-
-        massPounds = Math.floor((inputEl.value * convertLength) * 1000) / 1000
-        massKilograms = Math.floor((inputEl.value / convertLength) * 1000) / 1000
-        massEl.textContent = `${inputEl.value} kilograms = ${massPounds} pounds | ${inputEl.value} pounds = ${massKilograms} kilograms`
-    }
-    
-})
+}
